c1a2c9efac6ae5da66d7e4f1d6ac3672131903b1
abz0 <143391418+abz0@users.noreply.github.com>
2025-04-10 19:49:53 +1000

    bridge_exists_on_scene

--- Assets/Scripts/Bridges/BPInspector.cs
+++ Assets/Scripts/Bridges/BPInspector.cs
@@ -8,11 +8,10 @@ public class BPInspector : Editor
 {
     public override void OnInspectorGUI()
     {
-        string infoBox1 = "If there are any child gameobjects after loading up the unity file, delete them since the current script does not have them recorded after exiting the unity file.";
-        EditorGUILayout.HelpBox(infoBox1, MessageType.Warning);
-
-        string infoBox2 = "Once the gameobjects have been generated, make a change in the scene to save the generated objects";
-        EditorGUILayout.HelpBox(infoBox2, MessageType.Warning);
+        string infoBox1 = "Once the gameobjects have been generated, make a change in the scene to save the generated objects.";
+        string infoBox2 = "If there are any child gameobjects after loading up the unity file, delete them since the current script does not have them recorded after exiting the unity file.";
+        EditorGUILayout.HelpBox(infoBox1, MessageType.Info);
+        EditorGUILayout.HelpBox(infoBox2, MessageType.Info);
 
         base.OnInspectorGUI();
 
